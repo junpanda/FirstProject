9074df8815e03ccbfa9bda3081b794abc7b06c49
[Github <jungu2008@naver.com>
2022-06-27 14:33:49 +0900

    생성

---
+++ src/main/java/com/first/ex/test/TestController.java
@@ -0,0 +1,15 @@
+package com.first.ex.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String helloWorld() {
+		return "Hello World!";
+	}
+}
